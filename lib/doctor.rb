3334df3b6ce17cc6888701d969ea9083809cfb1c
David Gilbert <dagilbert333@gmail.com>
2019-06-22 02:28:58 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,15 +12,16 @@ class Doctor
     @@all
   end
   
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
   def appointments 
     Appointment.all.select do |appointment|
       appointment.doctor == self
     end
   end 
   
-  def new_appointment(date, patient)
-    Appointment.new(date, patient, self)
-  end
   
   def patients
     appointments.map do |appointment|
