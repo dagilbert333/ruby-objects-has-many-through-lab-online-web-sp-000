4baf3f972201d2c1d1e1f2c3e32032c92a1bc221
David Gilbert <dagilbert333@gmail.com>
2019-06-22 01:46:05 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,33 @@
+class Patient
+  attr_accessor :name, :appointment, :doctor
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+    
+  end
+  
+  def appointments 
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end 
+  
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+  
+  def doctors
+    Appointment.all.map do |appointment|
+      appointment.doctor
+    end
+  end
+  
+end
+
